76c1e30de836521e7e7fff1f9e5741dafe570625
Sebastián Rodríguez <sebastian.rodriguez@uruit.com>
2013-08-27 10:59:52 -0300

    Initial code commit.

---
+++ UruIT.ThinkTank.Demo/UruIT.ThinkTank.Demo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace UruIT.ThinkTank.Demo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
